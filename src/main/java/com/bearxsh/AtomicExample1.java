b6a2e414b9b7ff72436022311a59358a4b346ecc
Bearxsh <bearxsh@163.com>
2020-01-07 16:10:25 +0800

    增加请求总数

--- src/main/java/com/bearxsh/AtomicExample1.java
+++ src/main/java/com/bearxsh/AtomicExample1.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.LongAdder;
 public class AtomicExample1 {
 
     // 请求总数
-    public static int clientTotal = 5000;
+    public static int clientTotal = 6000;
 
     // 同时并发执行的线程数
     public static int threadTotal = 200;
